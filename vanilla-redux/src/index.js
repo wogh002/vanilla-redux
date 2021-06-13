f78a2f9767200c34176830545854b69de7be5681
wogh002 <ekekekem159@gmail.com>
2021-06-13 14:48:59 +0900

    completion vanilla-redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -22,9 +22,11 @@ const reducer = (state = [], action) => {
     console.log(action);
     switch (action.type) {
         case ADD_TODO:
-            return [{ text: action.text, id: Date.now() }, ...state];
+            const newToDoObj = {text: action.text, id: Date.now()}
+            return [newToDoObj, ...state];
         case DELETE_TODO:
-            return [];
+            return state.filter(toDo => toDo.id !== action.id);
+
         default:
             return state;
     }
@@ -37,7 +39,7 @@ const dispatchAddToDo = (text) => {
 }
 
 const dispatchDeleteToDo = e => {
-    const id = e.target.parentNode.id
+    const id = parseInt(e.target.parentNode.id);
     store.dispatch(deleteToDo(id));
 }
 const paintToDos = () => {
--- vanilla-redux/src/redux_concept.js
+++ vanilla-redux/src/redux_concept.js
@@ -7,15 +7,7 @@ const number = document.querySelector('span');
 const ADD = "ADD";
 const MINUS = "minus";
 
-
-//createStore(reducer(state,{action})) = state 보관장소
-//reducer = state 데이터 수정.
-//reducer 리턴하는 것은 전부 데이터(현재의 state)가 된다.
-//getState() 리듀서에서 리턴 한 state.
-//dispatch() action을 전달해준 후 reducer함수 실행한다 (리듀서의 두번쨰 인자)
 //subscribe(함수); status에 변화가 생기면 함수를 호출한다.
-//store 를 수정 할 수 있는 방법은 action을 사용하는 것.
-
 const countModidfier = (count = 0, action) => {
   switch (action.type) {
     case ADD:
